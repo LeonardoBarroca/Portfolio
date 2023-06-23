594d22c2d90eb4ad7f2bb3260da0f1a0b6f97a44
Leonardo Barroca <barroca.leonardo@gmail.com>
2023-06-22 21:30:01 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="app">
+    <Header />
+  </div>
+</template>
+
+<script>
+import Header from "@/components/Header.vue";
+
+export default {
+  components: { Header }
+};
+</script>
+
+<style scoped>
+.app {
+  font-family: 'Open Sans', sans-serif;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="navbar">
+        <ul class="navbar-menu">
+            <li><a href="#">Projeto 1</a></li>
+            <li><a href="#">Projeto 2</a></li>
+            <li><a href="#">Projeto 3</a></li>
+        </ul>
+    </div>
+</template>
+  
+<script>
+export default {
+    name: "Header"
+
+};
+</script>
+  
+<style scoped>
+.navbar {
+    background-color: #063940;
+    color: #fff;
+    display: flex;
+    align-items: center;
+    padding: 16px;
+}
+
+.navbar-menu {
+    display: flex;
+    list-style: none;
+    margin: 0;
+    padding: 0;
+}
+
+.navbar-menu li {
+    margin-right: 16px;
+
+}
+
+.navbar-menu li a {
+    text-decoration: none;
+    color: #fff;
+}
+
+@media (max-width: 768px) {
+    .navbar {
+        flex-direction: column;
+    }
+
+    .navbar-menu {
+        display: flex;
+        flex-direction: column;
+    }
+
+    .navbar-menu li {
+        margin-right: 0;
+    }
+
+    .navbar-menu>*:not(:last-child) {
+        margin-bottom: 16px;
+    }
+}
+</style>
+  
